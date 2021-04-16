44d2aaf625f9be4e7db260eb034642b39488b169
Lucas Zapico <zapicolucas@gmail.com>
2021-04-16 11:47:38 -0600

    enahnce phone and email link

--- src/components/links.js
+++ src/components/links.js
@@ -1,7 +1,22 @@
-import React from 'react';
-import { Box } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import {
+  Box,
+  IconButton,
+  useToast,
+  Tooltip,
+  Button,
+  Flex,
+  Link as ChLink,
+} from '@chakra-ui/react';
 import { Link } from 'gatsby';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
+import useClipboard from 'react-use-clipboard';
+import {
+  ArrowForwardIcon,
+  CopyIcon,
+  PhoneIcon,
+  EmailIcon,
+} from '@chakra-ui/icons';
+import { formatePhone, formatePhoneBot } from '../helpers/index';
 import LongArrow from '../assets/long-arrow.svg';
 
 export const LinkOne = (props) => {
@@ -44,5 +59,101 @@ export const LinkThree = ({ href, children }) => {
       <span className="wall front" />
       <span className="text">{children}</span>
     </Link>
+  );
+};
+
+export const LinkPhone = (props) => {
+  const { children } = props;
+  const formatedPhone = formatePhone(children);
+  const formatedPhoneBot = formatePhoneBot(children);
+  const [isCopied, setCopied] = useClipboard(formatedPhone, {
+    successDuration: 1000,
+  });
+  const toast = useToast();
+
+  return (
+    <Flex
+      {...props}
+      display="inline"
+      alignItems="center"
+      justifyContent="space-between"
+      minWidth="180px"
+    >
+      <Link fontWeight="semibold" mr={4} href={`tel:+1${formatedPhoneBot}`}>
+        <PhoneIcon mr={4} />
+        {formatedPhone}
+      </Link>
+      <Tooltip
+        label={isCopied ? 'Copied' : 'copy to clipboard'}
+        aria-label="copy to clipboard"
+        placement="right"
+      >
+        <IconButton
+          variant="none"
+          aria-label="copy to clipboard"
+          icon={<CopyIcon opacity="0.5" />}
+          onClick={
+            (setCopied,
+            () => {
+              return toast({
+                title: 'Copied to Clipboard',
+                description: `${formatedPhone} copied to clipboard`,
+                status: 'info',
+                variant: 'top-accent',
+                duration: 1000,
+                isClosable: true,
+              });
+            })
+          }
+        />
+      </Tooltip>
+    </Flex>
+  );
+};
+
+export const LinkEmail = (props) => {
+  const { children } = props;
+  const [isCopied, setCopied] = useClipboard(children, {
+    successDuration: 1000,
+  });
+
+  const toast = useToast();
+  return (
+    <Flex
+      {...props}
+      display="inline"
+      alignItems="center"
+      justifyContent="space-between"
+      minWidth="180px"
+    >
+      <Link fontWeight="semibold" mr={4} href={`mailto:${children}`}>
+        <EmailIcon mr={4} />
+        {children}
+      </Link>
+      <Tooltip
+        label={isCopied ? 'Copied' : 'copy to clipboard'}
+        aria-label="copy to clipboard"
+        placement="right"
+      >
+        <IconButton
+          variant="none"
+          aria-label="copy to clipboard"
+          icon={<CopyIcon opacity="0.5" />}
+          onClick={
+            (setCopied,
+            () => {
+              return toast({
+                title: 'Copied to Clipboard',
+                description: `${children} copied to clipboard`,
+                status: 'info',
+                variant: 'top-accent',
+                duration: 2000,
+                isClosable: true,
+              });
+            })
+          }
+        />
+      </Tooltip>
+    </Flex>
   );
 };
