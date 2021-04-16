49aec38d78a033c49f9cf6e7c6d7cc6d80b7e026
Lucas Zapico <zapicolucas@gmail.com>
2021-04-15 20:53:29 -0600

    add monkey scroll animations

--- src/components/scroll-animation.js
+++ src/components/scroll-animation.js
@@ -1,7 +1,7 @@
-import React, { useLayoutEffect, useRef, useState } from 'react';
+import React, { useLayoutEffect, useEffect, useRef, useState } from 'react';
 import { useSpring, animated as a } from 'react-spring';
 
-const ScrollAnimation = ({ children, props }) => {
+export const ScrollAniFadeIn = ({ children, props }) => {
   const [show, setShow] = useState(false);
   const action = useSpring({
     opacity: show ? 1 : 0,
@@ -46,4 +46,49 @@ const ScrollAnimation = ({ children, props }) => {
   );
 };
 
-export default ScrollAnimation;
+export const ScrollAniMonkey = ({ children, props, inOn = 0, outOn = 0 }) => {
+  const [show, setShow] = useState(false);
+  const action = useSpring({
+    opacity: show ? 1 : 0,
+  });
+
+  const ourRef = useRef(null);
+
+  const onScroll = () => {
+    const topPosition = (item) => item.getBoundingClientRect().top;
+    const itemPosition = topPosition(ourRef.current);
+    const offset = window.pageYOffset - itemPosition;
+    console.log('offset', offset);
+    console.log('on out', offset > outOn, show);
+    const scrollPosition = window.scrollY + window.innerHeight;
+    if (offset > inOn && offset < outOn) {
+      setShow((prev) => true);
+    } else if (offset > outOn) {
+      setShow((prev) => false);
+    } else {
+      setShow((prev) => false);
+    }
+  };
+
+  useEffect(() => {
+    const scrollPositionInit = window.scrollY + window.innerHeight;
+    const onLoad = () => {
+      const scrollPosition = window.scrollY + window.innerHeight;
+      console.log('load scroll post', scrollPosition);
+    };
+    window.addEventListener('load', onLoad);
+  }, []);
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  });
+  return (
+    <a.div style={action} ref={ourRef}>
+      {/* <div
+        className="test"
+        style={{ width: '100%', height: '2px', backgroundColor: 'red' }}
+      /> */}
+      {children}
+    </a.div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,68 +16,22 @@ import {
 import { graphql, Link } from 'gatsby';
 import Img from 'gatsby-image';
 import { ArrowForwardIcon } from '@chakra-ui/icons';
-import { Layout, ScrollAnimation, LinkThree } from '../components/index';
-
-const sessions = [
-  {
-    name: 'Single Session',
-    price: '$175',
-  },
-  {
-    name: 'Three Sessions',
-    price: '$475',
-    discount: '$50',
-  },
-  {
-    name: 'Six Sessions',
-    price: '$840',
-    discount: '$210',
-  },
-  {
-    name: 'Ten Sessions',
-    price: '$1315',
-    discount: '$435',
-  },
-];
-
-const Promotional = () => (
-  <Flex
-    minHeight="100px"
-    width="100%"
-    zIndex="0"
-    background="brand.six"
-    p={4}
-    justifyContent="space-between"
-  >
-    <Container maxWidth="container.xl">
-      <Flex
-        justifyContent="space-between"
-        alignItems="center"
-        width="100%"
-        flexDirection={{ base: 'column', md: 'row' }}
-      >
-        <Box m={4}>
-          <Heading as="h5" size="lg">
-            $50 off all sessions and packages for the month of&nbsp;April
-          </Heading>
-        </Box>
-        <Box m={4}>
-          <Link to="/schedule">
-            <Button size="lg" px={8} variant="btnTwo">
-              Schedule a Free Consultation
-            </Button>
-          </Link>
-        </Box>
-      </Flex>
-    </Container>
-  </Flex>
-);
+import {
+  Layout,
+  ScrollAniFadeIn,
+  ScrollAniMonkey,
+  LinkThree,
+} from '../components/index';
+import MonkeyOne from '../assets/monkey_1.svg';
+import MonkeyOneTwo from '../assets/monkey_1_2.svg';
+import MonkeyOneThree from '../assets/monkey_1_3.svg';
+import MonkeyTwo from '../assets/monkey_2.svg';
+import MonkeyThree from '../assets/monkey_3.svg';
 
 const Hero = ({ data }) => (
   <Flex
     flexDirection="column"
     justifyContent="space-between"
-    mb={20}
     minH="700px"
     zIndex="1"
   >
@@ -85,8 +39,8 @@ const Hero = ({ data }) => (
       <Box position="absolute" width="130vw">
         <Heading
           as="h3"
-          marginTop="-10%"
-          marginLeft="-10%"
+          marginTop="-5%"
+          marginLeft="-5%"
           opacity="0.1"
           fontSize={{
             base: '120',
@@ -99,19 +53,52 @@ const Hero = ({ data }) => (
         </Heading>
       </Box>
 
+      <Box
+        display={{ base: 'none', md: 'block' }}
+        position="absolute"
+        right="-20%"
+        bottom="0"
+        transform="scaleX(-1)"
+      >
+        <ScrollAniMonkey inOn="-300" outOn="-100">
+          <MonkeyOne />
+        </ScrollAniMonkey>
+      </Box>
+      <Box
+        display={{ base: 'none', md: 'block' }}
+        position="absolute"
+        right="10%"
+        bottom="0"
+        transform="scaleX(-1)"
+      >
+        <ScrollAniMonkey inOn="-300" outOn="0">
+          <MonkeyOneTwo />
+        </ScrollAniMonkey>
+      </Box>
+      <Box
+        display={{ base: 'none', md: 'block' }}
+        position="absolute"
+        right="50%"
+        bottom="0"
+        transform="scaleX(-1)"
+      >
+        <ScrollAniMonkey inOn="0" outOn="400">
+          <MonkeyOneThree />
+        </ScrollAniMonkey>
+      </Box>
       <Heading
         as="h1"
-        fontWeight="light"
         maxWidth="800px"
+        fontFamily="montas-semibold"
         fontSize={{
-          base: '120',
+          base: '100',
           sm: '120',
           md: '140',
           lg: '175',
         }}
-        mb={40}
+        mb={20}
       >
-        Blue Monkey Makes
+        Blue Monkey Makes....
       </Heading>
       <Box
         zIndex="-1"
@@ -124,175 +111,161 @@ const Hero = ({ data }) => (
       />
       {/* </a.div> */}
     </Container>
-    <Box p={10}>
+    <Flex justifyContent="space-between" flexWrap="wrap" bg="brand.five">
       <Container maxW="container.xl">
-        <Heading as="h3" size="2xl" lineHeight="base" maxWidth="600px" mb={10}>
-          We create custom digital presences that are engaging, performant and
-          SEO optimized.
-        </Heading>
-        <Heading as="h3" size="md" lineHeight="base">
-          Blue Monkey is a launch pad for entrepreneurs and small business. We
-          specialize in getting you coverage across branding, web presence,
-          development and business integrations.
-        </Heading>
+        <ScrollAniFadeIn>
+          <Box py={{ base: 0, sm: 20 }} flexBasis={{ base: '100%', md: '60%' }}>
+            <Box maxWidth="650px">
+              <Heading
+                as="h3"
+                size="3xl"
+                lineHeight="base"
+                mb={10}
+                color="brand.one"
+              >
+                ...custom digital presences that are engaging, performant, and
+                SEO optimized.
+              </Heading>
+            </Box>
+          </Box>
+        </ScrollAniFadeIn>
       </Container>
-    </Box>
+
+      {/* <Box
+        p={{ base: 0, sm: 10 }}
+        bg="brand.five"
+        minH="500px"
+        flexBasis={{ base: '100%', md: '40%' }}
+      >
+        <Container maxW="container.xl">
+          <Box maxWidth={{ base: '100%', md: '650px' }}>
+            <Heading as="h3" size="xl" lineHeight="base" color="brand.one">
+              Blue Monkey is a{' '}
+              <ChLink to="the-launch" color="brand.one">
+                launch pad for entrepreneurs and small business
+              </ChLink>
+              . We specialize in getting you coverage across{' '}
+              <ChLink to="why-branding" color="brand.one">
+                {' '}
+                branding
+              </ChLink>
+              ,{' '}
+              <ChLink to="why-branding" color="brand.one">
+                web presence
+              </ChLink>
+              ,{' '}
+              <ChLink color="brand.one" to="why-branding">
+                development
+              </ChLink>{' '}
+              and{' '}
+              <ChLink to="why-branding" color="brand.one">
+                business integrations
+              </ChLink>
+              .
+            </Heading>
+          </Box>
+        </Container>
+      </Box> */}
+    </Flex>
   </Flex>
 );
 
 const SectionOne = ({ data }) => (
-  <Box minH="500px" zIndex="1" py={10}>
-    <Container maxW="container.xl">
-      <Flex
-        // backgroundColor="brand.one"
-        flexDirection="row"
-        flexWrap="wrap"
-        justifyContent="space-between"
-        p={10}
-        position="relative"
+  <Box minH="500px" zIndex="1">
+    <Flex
+      // backgroundColor="brand.one"
+      flexDirection="row"
+      flexWrap="wrap"
+      justifyContent="space-between"
+      position="relative"
+    >
+      <Box
+        backgroundColor="brand.five"
+        p={{ base: 0, sm: 10 }}
+        height="auto"
+        flexBasis={{ base: '100%', md: '100%', lg: '40%' }}
+        pb={20}
       >
-        <Box flexBasis={{ base: '100%', md: '50%' }} mb={20}>
-          <ScrollAnimation>
+        <Container maxWidth="container.xl">
+          <ScrollAniFadeIn>
             <Box mb={10} mr={10}>
-              <Heading as="h3" size="2xl" lineHeight="base" zIndex="1" pb={10}>
+              <Heading
+                as="h3"
+                fontSize={{
+                  base: '90',
+                  sm: '110',
+                  md: '130',
+                  lg: '165',
+                }}
+                fontFamily="montas-semibold"
+                lineHeight="base"
+                zIndex="1"
+                color="brand.one"
+                pb={10}
+              >
                 Services
               </Heading>
-              <Heading size="xl" lineHeight="base">
-                We do a lot and that is intentional. We are a one stop shop for
-                brand, design, development, SEO and business&nbsp;integrations.
-              </Heading>
+              {/* <Heading color="brand.one">
+                We do it all so you don't have&nbsp;to.
+              </Heading> */}
             </Box>
-          </ScrollAnimation>
-          <ScrollAnimation>
-            <Heading as="h3" size="2xl" lineHeight="base" zIndex="1" pb={10}>
-              Readme
-            </Heading>
-            <Heading size="md" lineHeight="base">
-              We believe there is a threshold for being competitive in the
-              market and a lot of businesses spend good money and time trying to
-              get there. Worst case being mislead by agencies and firms that
-              charge more and deliver less. That's why we are who we are.
-            </Heading>
-          </ScrollAnimation>
-        </Box>
-        <Box flexBasis={{ base: '100%', md: '50%' }}>
-          <Box mb={6}>
-            <LinkThree href="/services/#branding">
-              Branding
-              <ArrowForwardIcon />
-            </LinkThree>
-          </Box>
-          <Box mb={6}>
-            <LinkThree href="/services/#design">
-              Design
-              <ArrowForwardIcon />
-            </LinkThree>
-          </Box>
-          <Box mb={6}>
-            <LinkThree href="/services/#development">
-              Development
-              <ArrowForwardIcon />
-            </LinkThree>
-          </Box>
-          <Box mb={6}>
-            <LinkThree href="/services/#seo-and-business-integrations">
-              SEO and Business Integrations
-              <ArrowForwardIcon />
-            </LinkThree>
-          </Box>
-        </Box>
-      </Flex>
-    </Container>
-  </Box>
-);
-
-const ProfileSection = ({ data }) => (
-  <Box minH="700px" py={20}>
-    <Container maxW="container.xl">
-      <Flex
-        flexDirection={{ base: 'column', sm: 'column', md: 'row' }}
-        alignItems={{ base: 'center', md: 'flex-start' }}
-        justifyContent="space-between"
-        flexGrow="1"
-        pos="relative"
-      >
-        <Box
-          zIndex="-1"
-          bg="brand.one"
-          height={{ base: '0%', md: '100%' }}
-          width="100%"
-          top="-10%"
-          left="-20%"
-          position="absolute"
-        />
-        <ScrollAnimation>
-          <Box
-            flexBasis="50%"
-            mb={20}
-            mr={{ md: 6 }}
-            pos="relative"
-            maxWidth="650px"
-          >
-            <Heading
-              fontWeight="light"
-              size="3xl"
-              as="h3"
-              lineHeight="base"
-              mb={6}
-            >
-              Hi, I'm Claire
-            </Heading>
-            <Heading as="h3" lineHeight="base" mb={6}>
-              My passion is to assist people to live empowered, fulfilled and
-              happy lives
-            </Heading>
-            <Text lineHeight="base" fontSize="xl">
-              As a graduate of the Hypnotherapy Academy of America, I specialize
-              using a client focused, results oriented approach to help create a
-              tangible and meaningful environment which is compassionate and
-              confidential.
-            </Text>
-          </Box>
-        </ScrollAnimation>
-        <ScrollAnimation>
-          <Box
-            flexBasis="50%"
-            position="relative"
-            width="100%"
-            ml={{ md: 6 }}
-            zIndex="0"
-          >
-            {' '}
-            {/* <Box
-            zIndex="0"
-            bg="brand.six"
-            height="100%"
-            width="100%"
-            top="-60px"
-            left="-20px"
-            position="absolute"
-          /> */}
-            <Img zIndex="1" fluid={data.profileImage.childImageSharp.fluid} />
-            <Text>
-              Claire Morency CHT | Connect@ClaireMorencyHypnotherapy.com |
-              415.747.1177
-            </Text>
-          </Box>
-        </ScrollAnimation>
-      </Flex>
-      <Box mt={20}>
-        <Heading as="h4" size="md">
-          Clinical Hypnotherapist Claire Morency offers hypnotherapy services in
-          Edmonds, WA.
-        </Heading>
+          </ScrollAniFadeIn>
+        </Container>
       </Box>
-    </Container>
+      <Box pb={20} flexBasis={{ base: '100%', md: '100%', lg: '60%' }}>
+        <ScrollAniFadeIn>
+          <Heading
+            p={{ base: 0, md: 20 }}
+            size="3xl"
+            m="auto"
+            lineHeight="base"
+            maxWidth="700px"
+          >
+            <Box
+              position="absolute"
+              right="-10%"
+              bottom="0"
+              transform="scaleX(1)"
+            >
+              {/* <MonkeyThree /> */}
+            </Box>
+            We are a one stop shop for frugal and competitive:
+          </Heading>
+          <Container maxWidth="container.xl" maxWidth="700px">
+            <Box mb={6}>
+              <LinkThree href="/services/#branding">
+                Branding
+                <ArrowForwardIcon />
+              </LinkThree>
+            </Box>
+            <Box mb={6}>
+              <LinkThree href="/services/#design">
+                Design
+                <ArrowForwardIcon />
+              </LinkThree>
+            </Box>
+            <Box mb={6}>
+              <LinkThree href="/services/#development">
+                Development
+                <ArrowForwardIcon />
+              </LinkThree>
+            </Box>
+            <Box mb={6}>
+              <LinkThree href="/services/#seo-and-business-integrations">
+                SEO and Business Integrations
+                <ArrowForwardIcon />
+              </LinkThree>
+            </Box>
+          </Container>
+        </ScrollAniFadeIn>
+      </Box>
+    </Flex>
   </Box>
 );
 
 const FAQSection = ({ data }) => (
   <Container maxW="container.xl">
-    <ScrollAnimation>
+    <ScrollAniFadeIn>
       <Flex flexWrap="wrap" minH="700px" py={40} flexGrow="1">
         <Box
           minHeight="400px"
@@ -356,17 +329,17 @@ const FAQSection = ({ data }) => (
           <Heading as="h3" lineHeight="base" mb={6} minHeight="150px">
             <a id="what-hypnotherapy-can-do">What Hypnotherapy Can Do?</a>
           </Heading>
-          <Text lineHeight="base" fontSize="xl">
+          <Box lineHeight="base" fontSize="xl">
             <UnorderedList>
               <ListItem>Enthusiasm for Life</ListItem>
               <ListItem>Build Confidence and Purpose</ListItem>
               <ListItem>Achieve Business Goals</ListItem>
               <ListItem>Increase Quality of Sleep</ListItem>
             </UnorderedList>
-          </Text>
+          </Box>
         </Box>
       </Flex>
-    </ScrollAnimation>
+    </ScrollAniFadeIn>
   </Container>
 );
 
