3c3f4febaea81a71a2ce0225e944203d3895453a
Lucas Zapico <zapicolucas@gmail.com>
2021-09-17 09:17:32 -0600

    fix: double font import

--- src/theme/fonts.js
+++ src/theme/fonts.js
@@ -11,13 +11,6 @@ const Fonts = () => (
       font-weight: normal;
       font-style: normal;
     }
-    @font-face {
-      font-family: 'montas-regular';
-      src: url('/montas-regular-webfont.woff2') format('woff2'),
-           url('/montas-regular-webfont.woff') format('woff');
-      font-weight: normal;
-      font-style: normal;
-    }
   `}
   />
 );
